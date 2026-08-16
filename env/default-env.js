0d945ffdd568f33b637217afa696e707e6818c18
algorifer <algorifer@gmail.com>
2026-08-16 11:11:13 +0400

    fix: default env

--- env/default-env.js
+++ env/default-env.js
@@ -1,7 +1,7 @@
 export const env = {
-  queueMicrotask,
-  setTimeout,
-  clearTimeout,
-  setInterval,
-  clearInterval,
+  queueMicrotask: (...a) => globalThis.queueMicrotask (...a),
+  setTimeout: (...a) => globalThis.setTimeout (...a),
+  clearTimeout: (...a) => globalThis.clearTimeout (...a),
+  setInterval: (...a) => globalThis.setInterval (...a),
+  clearInterval: (...a) => globalThis.clearInterval (...a),
 }
